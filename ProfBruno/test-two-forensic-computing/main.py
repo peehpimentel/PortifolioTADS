dc9b404e47bb823caebd23100d779b82319eaeff
Pedro Henrique Mello <pmello.ads@gmail.com>
2024-06-12 06:03:28 -0400

    Changing the comments for PT-BR to English

--- ProfBruno/test-two-forensic-computing/main.py
+++ ProfBruno/test-two-forensic-computing/main.py
@@ -2,40 +2,40 @@ import os
 import subprocess
 import csv
 
-# Tratando o caminho do exiftool como string literal
+# Using exiftool path as literal string.
 exiftool = r"C:\Windows\exiftool.exe"
 
-# Função para extrair metadados de arquivos DOCX
+# Function to extract metadata from files.
 def extract_metadata(filepath):
     try:
         result = subprocess.run([exiftool, '-json', filepath], capture_output=True, text=True, check=True)
         metadata = result.stdout
         return metadata
     except subprocess.CalledProcessError as err:
-        # Esse f antes da string é uma forma de formatted string literal que serve para incorporar expressões e variáveis dentro de strings.
+        # This f before the string is a formatted string literal, it helps incorporate expressions and variables into strings.
         print(f"Error trying to execute exiftool: {err}")
         return None
 
-# Função principal
+# Main function.
 def main():
-    # Obter o caminho da pasta do usuário
+    # Get user's folder.
     folder_path = input("Enter the folder path: ")
 
-    # Verificar se o caminho da pasta é valido
+    # Verify if the folder's path is valid.
     if not os.path.isdir(folder_path):
         print("Wrong path. Please, try entering a valid path.")
         return
     
-    # Definir os tipos de arquivos suportados
+    # Define the supported extensions.
     supported_extensions = ['.docx', '.txt', '.pdf', '.jpg', '.jpeg', '.png', '.gif', '.wav', '.mp3', '.ogg', '.mp4', '.mov', '.avi', '.mkv']
 
-    # Criar arquivo CSV para salvar os metadados
+    # Creating a CSV file to save metadata.
     output_file = os.path.join(folder_path,"metadados.csv")
     with open(output_file, "w", newline="", encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['File', 'Metadata'])
 
-        # Extrair e salvar metadados de cada arquivo na pasta
+        # Extracting and saving every file's metadata into the folder.
         for root, dirs, files in os.walk(folder_path): 
             for filename in files: 
                 filepath = os.path.join(root, filename)
